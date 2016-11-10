78df90a2b0e2a51d0f481b4bcf19d1db2e4cdf41
Alexey Berdnikov <berdnikov_ay@bunkermedia.ru>
2016-11-10 10:43:10 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Larakit.
+ * Link: http://github.com/larakit
+ * User: Alexey Berdnikov
+ * Date: 23.05.16
+ * Time: 10:02
+ */
+\Larakit\StaticFiles\Manager::package('larakit/sf-angular-resource')
+                            ->setSourceDir('public')
+                            ->usePackage('larakit/sf-angular')
+                            ->jsPackage('angular-resource.js');
